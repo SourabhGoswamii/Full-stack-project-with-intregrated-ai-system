24ad0e5db235de8b29ce61a603eabab0fa55da1c
TR Goswami <qc7@zll.local>
2025-01-23 22:24:57 +0530

    sec

--- BACKEND/Middleware/auth.middelware.js
+++ BACKEND/Middleware/auth.middelware.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 
-
 export const authUser = async (req, res, next) => {
     try {
-        const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
-
+        const token =
+            req.cookies?.token ||
+            ( req.headers.authorization.split(' ')[1]);
+        
         if (!token) {
             return res.status(401).send({ error: 'Unauthorized User' });
         }
@@ -13,9 +14,7 @@ export const authUser = async (req, res, next) => {
         req.user = decoded;
         next();
     } catch (error) {
-
-        console.log(error);
-
+        console.error("JWT Authentication Error:", error.message);
         res.status(401).send({ error: 'Unauthorized User' });
     }
-}
+};
